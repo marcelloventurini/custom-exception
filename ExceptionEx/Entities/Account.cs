299d6d42d1664abfc6578fa3920cc71ad4e7d19b
Marcello Venturini <75899197+marcelloventurini@users.noreply.github.com>
2023-06-06 17:36:05 -0300

    Add files via upload

---
+++ ExceptionEx/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿using ExceptionEx.Entities.Exceptions;
+
+namespace ExceptionEx.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance < amount)
+            {
+                throw new DomainException("Saldo insuficiente.");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("A quantia informada excede o limite de saque.");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ ExceptionEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExceptionEx.Entities;
+using ExceptionEx.Entities.Exceptions;
+
+try
+{
+    Console.WriteLine("Informe os dados da conta:");
+    Console.Write("Número: ");
+    int number = int.Parse(Console.ReadLine());
+
+    Console.Write("Titular: ");
+    string holder = Console.ReadLine();
+
+    Console.Write("Saldo inicial: ");
+    double balance = double.Parse(Console.ReadLine());
+
+    Console.Write("Limite de saque: ");
+    double withdrawLimit = double.Parse(Console.ReadLine());
+
+    Account account = new(number, holder, balance, withdrawLimit);
+
+    Console.Write("Informe a quantia que deseja sacar: ");
+    double withdraw = double.Parse(Console.ReadLine());
+
+    account.Withdraw(withdraw);
+
+    Console.WriteLine($"Novo saldo: {account.Balance:F2}");
+}
+catch (DomainException e)
+{
+    Console.WriteLine($"Erro: {e.Message}");
+}
